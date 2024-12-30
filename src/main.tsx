02cd9e4c08e9dc2d452e2f2b1156d89b07a4f449
swqa7697 <swqa7697@gmail.com>
2024-12-30 10:03:00 -0800

    Initialized project directory structure and added necessary dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './routes/home';
+
+import './assets/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
---
+++ src/routes/home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>HBR HD Wiki</h1>
+    </div>
+  );
+}
+
+export default Home;
