27e9b071bd8da2fec7d78c8cd40e38341628f9f3
swqa7697 <swqa7697@gmail.com>
2025-01-05 18:31:07 -0800

    Test function 4

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
   const method = req.method;
 
   switch (method) {
