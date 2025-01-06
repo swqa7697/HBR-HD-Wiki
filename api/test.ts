322503269cc587fe5d9681434b1917c6e95b6f11
swqa7697 <swqa7697@gmail.com>
2025-01-05 22:58:03 -0800

    Test function 19

---
+++ api/test.ts
@@ -0,0 +1,30 @@
+export async function POST(request: Request) {
+  try {
+    const { val = 'No Value' } = await request.json();
+    return new Response(
+      JSON.stringify({
+        val,
+        Method: request.method,
+      }),
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  } catch (err) {
+    return new Response(
+      JSON.stringify({
+        Message: 'Error parsing post request body',
+        err,
+      }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  }
+}
