a2627c07f38bd761d36841441e25f86a835f1603
swqa7697 <swqa7697@gmail.com>
2025-01-05 19:39:21 -0800

    Test functions 7

--- api/hello.ts
+++ api/hello.ts
@@ -1,21 +1,15 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export const config = {
-  runtime: 'nodejs',
-};
-
-export default function (request: VercelRequest, response: VercelResponse) {
-  const method = request.method;
-
-  switch (method) {
-    case 'GET': {
-      const { name = 'World' } = request.query;
-      response.status(200).json({ message: `Hello ${name}!` });
-      break;
-    }
-    default: {
-      response.status(405).json({ message: `Method ${method} Not Allowed` });
-      break;
-    }
-  }
+export function GET(request: Request) {
+  const url = new URL(request.url);
+  const name = url.searchParams.get('name') ?? 'World';
+  return new Response(
+    JSON.stringify({
+      message: `Hello ${name}!`,
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
 }
