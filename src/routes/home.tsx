45054be72472b4636b7eef2064e3f9b1a968aada
swqa7697 <swqa7697@gmail.com>
2025-01-07 11:04:04 -0800

    Added stickers assets

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { base_title, home_title } from '../util/titles.json';
+import { Card } from 'flowbite-react';
 
 function Home() {
   useEffect(() => {
@@ -7,11 +8,37 @@ function Home() {
   }, []);
 
   return (
-    <div className="p-2">
-      <h1 className="text-center text-black">HOME</h1>
-      <a href="https://google.com" target="_blank" rel="noopener noreferrer">
-        Google (test)
-      </a>
+    <div className="flex flex-1 md:flex-wrap w-full p-5 items-center justify-start md:flex-row flex-col gap-5">
+      <Card
+        className="max-w-sm bg-pink-800/75 border-4 border-black shadow-lg"
+        renderImage={() => <img src="assets/logo.webp" className="h-10" />}
+        horizontal
+      >
+        <h1 className="text-center text-black">HOME</h1>
+        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+          Google (test)
+        </a>
+      </Card>
+      <Card
+        className="bg-pink-800/75 border-4 border-black shadow-lg"
+        renderImage={() => <img src="assets/logo.webp" className="h-10" />}
+        horizontal
+      >
+        <h1 className="text-center text-black">HOME</h1>
+        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+          Google (test)
+        </a>
+      </Card>
+      <Card
+        className="bg-pink-800/75 border-4 border-black shadow-lg"
+        renderImage={() => <img src="assets/logo.webp" className="h-10" />}
+        horizontal
+      >
+        <h1 className="text-center text-black">HOME</h1>
+        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+          Google (test)
+        </a>
+      </Card>
     </div>
   );
 }
